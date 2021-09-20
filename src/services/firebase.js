872e310bc97156d5210b259e3f3f9ea78c2ffb7a
Jaysc <jaysc2@gmail.com>
2021-09-20 11:53:52 +0100

    deploy tweaks

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,21 +6,21 @@ import Authentication from './authentication';
 import Database from './database';
 import DatabaseLogic from './database-logic';
 
+const isDevelop = process.env.NODE_ENV === 'develop';
+
 const config = {
   apiKey: process.env.REACT_PUBLIC_FIREBASE_API_KEY,
   authDomain: `${process.env.REACT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  databaseURL: process.env.ENVIRONMENT === 'develop' ? 'http://localhost:9000/?ns=jaysc-wwrando-tracker-default-rtdb' : `https://${process.env.REACT_PUBLIC_FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`,
+  databaseURL: isDevelop ? 'http://localhost:9000/?ns=jaysc-wwrando-tracker-default-rtdb' : `https://${process.env.REACT_PUBLIC_FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`,
   projectId: process.env.REACT_PUBLIC_FIREBASE_PROJECT_ID,
 };
-
-const isLocal = location.hostname === 'localhost';
 
 export default class Firebase {
   static async initialize(permaId, gameId) {
     initializeApp(config);
 
-    const auth = Authentication.initialize(isLocal);
-    const database = Database.initialize(isLocal, permaId, gameId);
+    const auth = Authentication.initialize(isDevelop);
+    const database = Database.initialize(isDevelop, permaId, gameId);
     await Promise.all([auth, database]);
 
     DatabaseLogic.init();
